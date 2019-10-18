a8e4be117f7c8162741c39e6779f8be7e04d5ce3
Jeong Woo Tae <wjddnxo1478@naver.com>
2019-10-18 18:01:41 +0900

    add new projects

---
+++ WPF_ListBox/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_ListBox
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void GetIndex(object sender, RoutedEventArgs e)
+        {
+            ListBoxItem lbi = (ListBoxItem)(lb.ItemContainerGenerator.ContainerFromIndex(0));
+            btn.Content = "The contents of the item at index 0 are : " + (lbi.Content.ToString()) + ".";
+        }
+
+    }
+}
