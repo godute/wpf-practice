ea4be5abf6d417f33f7689fea113574a36054133
godute <wjddnxo1478@naver.com>
2019-10-20 15:08:12 +0900

    add menuitem

---
+++ WPF_Style/TimeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WPF_Style
+{
+    class TimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return String.Format("{0:hh시 MM분 ss초}", value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
